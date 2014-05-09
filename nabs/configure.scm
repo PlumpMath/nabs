340ff4c9b9d32573d1c8f71d18a7223346a2c0b9
Damien Leroux <damien.leroux@gmail.com>
2014-05-10 00:46:48 +0200

    Added search path vhash.

--- nabs/configure.scm
+++ nabs/configure.scm
@@ -2,7 +2,8 @@
                :use-module (nabs tools)
                :use-module (nabs find)
                :use-module (nabs predicates)
-               :use-module (ice-9 match)
+               ;:use-module (ice-9 match)
+               :use-module (ice-9 vlist)
                :use-module (srfi srfi-1)
                :use-module (gnu make)
                :re-export (checks)
@@ -10,25 +11,52 @@
                :re-export (find-file-by-path-suffix)
                :re-export (find-file-by-exact-name)
                :export (declare
+                        add-search-path
                         configure-include
                         configure-frontend
                         write-frontend
                         ))
 
+; export predicates
+;(module-for-each (lambda (k v) (print k " --> " v "\n")(re-export (k v))) (resolve-interface '(nabs predicates)))
+
 (define configurables '())
 (define-public (dump-configurables) configurables)
 
-(define (declare name make-var-name search-path find-mode-tag find-target predicates)
+(define ~search-path vlist-null)
+(define-public (dump-search-path) (vhash-fold-right (lambda (k v r) (cons (list k v) r)) '() ~search-path))
+
+(define (add-search-path tag . path)
+  (let ((exists? (vhash-assoc tag ~search-path)))
+    (set! ~search-path
+      (if exists?
+        (vhash-fold-right (lambda (k v r)
+                            (if (equal? k tag)
+                              (vhash-cons k (append v path) r))) vlist-null ~search-path)
+        (vhash-cons tag path ~search-path)))))
+
+(define (get-search-path tag)
+  (let ((exists? (vhash-assoc tag ~search-path)))
+    (if exists?
+      (cdr exists?)
+      (begin (print (dump-search-path) "\n")
+      (error (concat "Search path for '" (symbol->string tag) " was not defined!"))))))
+
+(define (declare tag name make-var-name find-mode-tag find-target predicates)
   (set! configurables
         (append configurables
                 (list (list make-var-name
                             (lambda () (pick-unique name
                                                     (filter predicates
-                                                            (find-file search-path
+                                                            (find-file (get-search-path tag)
                                                                        find-mode-tag
                                                                        find-target))
                                                     predicates)))))))
+
+;; RENDER CONFIGURATION
+
 (define (eval-configurables)
+  "Turn the list of configurables into a string that can be directly fed to Make."
   (string-concatenate
     (map (lambda (p) (concat (car p) "=" ((cadr p)) "\n"))
          configurables)))
@@ -57,15 +85,18 @@ include .configuration
     ;(print "project-path " project-path "\n")
     (gmk-eval (concat "
 .configure:
-\t$(guile (write-frontend \"Makefile\" \"" project-path "\" '(" makefiles ")))
+\t@$(guile (write-frontend \"Makefile\" \"" project-path "\" '(" makefiles ")))
 
 "))))
 
 (define (write-frontend filename vpath makefiles)
-  (let ((f (open-output-file filename)))
-    (display (eval-configurables) f)
-    ;(print "vpath " vpath " vpath " vpath "\n")
-    (display (concat "vpath % " vpath "\n") f)
-    (display (string-join (map (lambda (m) (concat "include " (symbol->string m))) makefiles) "\n" 'infix) f)
-    (close f)))
+  (if (false-if-exception
+        (let ((f (open-output-file filename)))
+          (display (eval-configurables) f)
+          ;(print "vpath " vpath " vpath " vpath "\n")
+          (display (concat "vpath % " vpath "\n") f)
+          (display (string-join (map (lambda (m) (concat "include " (symbol->string m))) makefiles) "\n" 'infix) f)
+          (close f)))
+    "true"
+    "false"))
 
